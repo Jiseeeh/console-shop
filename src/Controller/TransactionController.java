d40d6bc0576c9f057dcee48fb6bd4daf283d7527
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-15 23:23:11 +0800

    Build partial of Transaction

--- src/Controller/TransactionController.java
+++ src/Controller/TransactionController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import Model.Transaction;
+import View.TransactionView;
+
 public class TransactionController {
+    private Transaction transaction;
+    private TransactionView transactionView = new TransactionView();
+
+    public  TransactionController (Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public void startTransaction () {
+
+    }
 }
--- src/Model/Transaction.java
+++ src/Model/Transaction.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class Transaction {
+    private final Double customerBalance;
+    private final Double productPrice;
+
+    public Transaction(Double customerBalance, Double productPrice) {
+        this.customerBalance = customerBalance;
+        this.productPrice = productPrice;
+    }
+
+    public Double getCustomerBalance() {
+        return customerBalance;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
 }
