782c1b5144785e137739d2b3ee556cf33885c55a
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-15 23:23:30 +0800

    Build product

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -1,4 +1,25 @@
 package Model;
 
 public class Product {
+    private final String PRODUCT_NAME;
+    private final Double PRODUCT_PRICE;
+    private final Integer PRODUCT_QUANTITY;
+
+    public Product(String productName, Double productPrice, Integer productQuantity) {
+        this.PRODUCT_NAME = productName;
+        this.PRODUCT_PRICE = productPrice;
+        this.PRODUCT_QUANTITY = productQuantity;
+    }
+
+    public String getProductName() {
+        return PRODUCT_NAME;
+    }
+
+    public Double getProductPrice() {
+        return PRODUCT_PRICE;
+    }
+
+    public Integer getProductQuantity() {
+        return PRODUCT_QUANTITY;
+    }
 }
