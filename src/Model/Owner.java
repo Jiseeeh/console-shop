b6d10c27280ca152b3b4f0098b9885872c8f7065
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-17 17:44:35 +0800

    Make construcotr of Owner private

--- src/Model/Owner.java
+++ src/Model/Owner.java
@@ -7,16 +7,11 @@ public class Owner extends User {
     public static final List<Customer> CUSTOMERS_LIST = new ArrayList<>();
     public static final List<Product> PRODUCT_LIST = new ArrayList<>();
     public static final List<Transaction> TRANSACTION_LIST = new ArrayList<>();
-    private final List<Customer> BLOCKED_CUSTOMERS_LIST = new ArrayList<>();
 
-    public Owner() {
+    private Owner() {
         this.setFirstName("JohnCarlo");
         this.setLastName("Camara");
         this.setUsername("owner");
         this.setPassword("123");
-    }
-
-    public List<Customer> getBlockedCustomersList() {
-        return BLOCKED_CUSTOMERS_LIST;
     }
 }
