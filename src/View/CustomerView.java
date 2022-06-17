36a05eb8761ac462e507b250ad644d728b3878f6
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-17 17:45:16 +0800

    Add view/clear of cart in customer view

--- src/View/CustomerView.java
+++ src/View/CustomerView.java
@@ -1,29 +1,54 @@
 package View;
 
+import Helper.UIHelper;
+import Helper.ValidationHelper;
 import Model.Customer;
+import Model.Product;
+
+import java.util.List;
 
 public class CustomerView {
     public void showCustomerDashboard() {
         System.out.println("""
+                                
                 What do you want to do?
                 1 -> Cash in
                 2 -> Go shopping (can add to cart or buy)
                 3 -> View Info
                 4 -> View balance
-                5 -> Checkout
-                6 -> Logout
+                5 -> View my cart
+                6 -> Clear my cart
+                7 -> Checkout
+                8 -> Logout
                 """);
         System.out.print(": ");
     }
 
     public void viewMyInfo(Customer customer) {
+        System.out.println("\n----------------------------");
         System.out.printf("""
                 Firstname : %s
                 Lastname : %s
                 """, customer.getFirstName(), customer.getLastName());
+        System.out.println("----------------------------");
     }
 
     public void viewMyBalance(Customer customer) {
-        System.out.println(customer.getBalance());
+        UIHelper.sleep(1, "Balance: P" + customer.getBalance());
+    }
+
+    public void viewMyCart(List<Product> customerCart) {
+        if (ValidationHelper.isCartEmpty(customerCart)) return;
+
+        System.out.println("** YOUR CART **");
+        customerCart.forEach(cart -> {
+            System.out.println("\n----------------------------");
+            System.out.printf("""
+                    Product name: %s
+                    Product price: %.1f
+                    Quantity: %d
+                    """, cart.getProductName(), cart.getProductPrice(), cart.getBOUGHT_QUANTITY());
+            System.out.println("----------------------------");
+        });
     }
 }
